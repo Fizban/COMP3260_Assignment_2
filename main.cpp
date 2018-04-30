0ee9a6b5e26ec70a3a91e87ba6dce718ea4b1e16
SebastianWebster <Walkawayinka123>
2018-04-30 19:48:14 +1000

    Commit test1

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 using namespace std;
-
+/* https://github.com/Fizban/COMP3260_Assignment_2/commits/master
+ * 1.Fileread/Write
+ * 2.Parser
+ * 3.E-Table
+ * 4.XOR
+ * 5.S-Box
+ * 6.Permutate
+ * 7.Avalanche Analysis
+ *
+ *
+ */
 int main() {
     string argument;
     cin >> argument;
